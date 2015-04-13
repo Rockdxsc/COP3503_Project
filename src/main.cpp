9afd871affe2f810cddd22fecfcb6c5a16356dac
Harsha Kuchampudi <harshakuchampudi@gmail.com>
2015-04-13 10:39:14 -0400

    Added Comments

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ int main() {
     string userDirection;
     Player mainPlayer;
 
+    // Initialize the Items that are Available in the Level
     vector<string> itemsList;
     itemsList.push_back("Item1");
     itemsList.push_back("Item2");
@@ -19,11 +20,15 @@ int main() {
     itemsList.push_back("Item5");
     itemsList.push_back("Item6");
 
+    // Generate an int Map Based on the Items in 'itemsList' and Create 40 Enemies
     vector< vector<int> > integerMap = generateIntMap(itemsList, 40);
+
+    // Generate a Player Map Based on the Generated Integer Map
     vector< vector<string> > playerMap = generatePlayerMap(integerMap);
 
+    // Game/Level Loop
     while (userAction != "EXIT") {
-        
+
         printPlayerMap(playerMap);
         cout << "What Would You Like to Do: ";
         cin >> userAction;

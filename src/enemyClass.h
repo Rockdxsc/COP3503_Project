d1a7b49e893564dc063a3669c6a9fbd15b6977b3
Kevin Wong <1996kevinwong@gmail.com>
2015-04-19 17:32:18 -0400

    Update enemyClass.h
    Added declarations for each enemy class and fixed the variable names to accommodate the respective enemy

--- src/enemyClass.h
+++ src/enemyClass.h
@@ -72,6 +72,127 @@ public:
 
 };
 
+ class Goblin{
+
+private:
+
+    int goblinHealth;
+    int goblinLevel;
+
+public:
+
+ Goblin(){
+
+        int gLevel;     // Stores the Randomized Goblin's Level
+        int gHealth;    // Stores the Calculated Health
+        int gLevelMax;  // Max Level Range
+        int gLevelMin;  // Min Level Range
+
+        // Random Seed
+        srand (time(NULL));
+
+        // Generate Random Level Between 2 and 11
+        gLevel = 2 + (rand() % (int)(10 - 1 + 1));
+
+        // Random Health Based on Level
+        gLevelMax = gLevel * 10;
+        gLevelMin = (gLevel * 10) - 10;
+        gHealth = gLevelMin + (rand() % (int)(gLevelMax - gLevelMin + 1));
+
+        // Generate Goblin Level and Health
+        goblinLevel = gLevel;
+        goblinHealth = gHealth;
+
+    }
+
+    // Deal Damage to Goblin's Health
+    void dealDamage(int damageDealt){
+        goblinHealth = goblinHealth - damageDealt;
+    }
+
+    // Returns the goblin's Health
+    int returnHealth(){
+        return goblinHealth;
+    }
+
+    // Returns the goblin's Level
+    int returnLevel(){
+        return goblinLevel;
+    }
+
+    // Randomly Generated Attack Strength
+    int attack(){
+
+        // Generate Random Seed
+        srand (time(NULL));
+
+        int attackPower = (int)(goblinLevel/2) + (rand() % (int)((goblinLevel+2) - (goblinLevel/2) + 1));
+        return attackPower;
+
+    }
+};
+
+
+class Orc{
+
+private:
+
+    int orcHealth;
+    int orcLevel;
+
+public:
+
+     Orc(){
+
+        int oLevel;     // Stores the Randomized Orc's Level
+        int oHealth;    // Stores the Calculated Health
+        int oLevelMax;  // Max Level Range
+        int oLevelMin;  // Min Level Range
+
+        // Random Seed
+        srand (time(NULL));
+
+        // Generate Random Level Between 3 and 12
+        oLevel = 3 + (rand() % (int)(10 - 1 + 1));
+
+        // Random Health Based on Level
+        oLevelMax = oLevel * 10;
+        oLevelMin = (oLevel * 10) - 10;
+        oHealth = oLevelMin + (rand() % (int)(oLevelMax - oLevelMin + 1));
+
+        // Generate Orc Level and Health
+        orcLevel = oLevel;
+        orcHealth = oHealth;
+
+    }
+
+    // Deal Damage to Orc's Health
+    void dealDamage(int damageDealt){
+        orcHealth = orcHealth - damageDealt;
+    }
+
+    // Returns the Orc's Health
+    int returnHealth(){
+        return orcHealth;
+    }
+
+    // Returns the Orc's Level
+    int returnLevel(){
+        return orcLevel;
+    }
+
+    // Randomly Generated Attack Strength
+    int attack(){
+
+        // Generate Random Seed
+        srand (time(NULL));
+
+        int attackPower = (int)(orcLevel/2) + (rand() % (int)((orcLevel+2) - (orcLevel/2) + 1));
+        return attackPower;
+
+    }
+};
+
 class Boss{
 
 private:
